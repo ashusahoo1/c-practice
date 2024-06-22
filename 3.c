e8f30e7583d7ba2725ce46d17bae6c2533ae6471
animesh sahoo <animeshsahoo159@gmail.com>
2024-06-22 12:08:51 +0530

    // convert celsisus into farenheit

---
+++ 3.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{ float celsius;
+printf("what is the temperature in celsius\n");
+scanf("%f", &celsius);
+
+printf("the temp in farenheit is %f", celsius*1.8 + 32);
+
+    return 0;
+}
